896b3e5e9417dcd5148df73ba640d41f271a2fd8
Ivo Fritsch <averbeckfritsch@gmail.com>
2018-07-01 19:18:31 -0300

    Added the main class

---
+++ src/htmltopdf/HtmlToPdf.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package htmltopdf;
+
+/**
+ * This is the entry class for the converter API.
+ * Contains all the functions you will use.
+ * 
+ * @author Ivo
+ */
+public class HtmlToPdf {
+    
+}
